d39a1390e61aad4aae290e4112ef6227a3080c42
emdarcher <emd519@yahoo.com>
2014-03-22 22:53:23 -0700

    got templates and views somewhat setup and working urls

--- scouting_project/scouting/views.py
+++ scouting_project/scouting/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+#got to import models 
+
+
 # Create your views here.
+
+def index(request):
+    
+    test_string = "Hello, I am a test string."
+    
+    context = {'test_string_context': test_string}
+
+    return render(request, 'scouting/index.html', context) 
--- scouting_project/scouting_project/urls.py
+++ scouting_project/scouting_project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-import scouting
+#import scouting
 
 admin.autodiscover()
 
@@ -11,5 +11,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^scouting/', include(scouting.urls)),
+    url(r'^scouting/', include('scouting.urls')),
 )
