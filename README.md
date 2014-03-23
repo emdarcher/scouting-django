4df3bb35e4bc1943f3cb758e5cc8618583238d71
emdarcher <emd519@yahoo.com>
2014-03-22 21:08:54 -0700

    set stuff up and added readme, also first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+a web app to use for scouting FIRST robotics competintions.
+
+in  development for FRC Team 4159, cardinalbotics
+
+
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sethan/programming/robotics/scouting-webapp/scouting-django/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
