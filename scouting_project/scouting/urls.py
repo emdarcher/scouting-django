87d85b8e73d67321e8dc51b0727d969f33922462
emdarcher <emd519@yahoo.com>
2014-03-24 09:12:40 -0700

    made teamdata_list template and it is working

--- scouting_project/scouting/urls.py
+++ scouting_project/scouting/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import patterns, url
 from scouting import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+
+    #adding url for teamdata_list page
+    url(r'^teamdata_list/$', views.teamdata_list, name='teamdata_list'),
 
 
 )
