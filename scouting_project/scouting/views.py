3e85fdd9e2902d06446ce1463b4c913c15672316
emdarcher <emd519@yahoo.com>
2014-03-24 08:55:31 -0700

    added teamdata_list view, still needs a template i think

--- scouting_project/scouting/views.py
+++ scouting_project/scouting/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 #got to import models 
-
+from scouting.models import TeamData
 
 # Create your views here.
 
@@ -12,6 +12,20 @@ def index(request):
     
     test_string = "Hello, I am a test string."
     
+        
+    
     context = {'test_string_context': test_string}
 
-    return render(request, 'scouting/index.html', context) 
+    return render(request, 'scouting/index.html', context)
+
+def teamdata_list(request):
+
+    #creates a list with TeamData ordered by the match number 
+    list_by_match = TeamData.objects.order_by('-match_number')[0:]
+
+    #creates the context
+    context = {'list_by_match_context':list_by_match}
+
+    return render(request, "scouting/teamdata_list.html", context)
+
+
