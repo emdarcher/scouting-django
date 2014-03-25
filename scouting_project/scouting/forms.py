b0c776b1647fdec2297fb5e8753bf4f3f1532717
emdarcher <emd519@yahoo.com>
2014-03-24 21:59:09 -0700

    added more help_text to form, and adjusted formatting

--- scouting_project/scouting/forms.py
+++ scouting_project/scouting/forms.py
@@ -4,8 +4,20 @@ from django import forms
 from scouting.models import TeamData
 
 class TeamDataForm(forms.ModelForm):
-    #scouter_name = models.CharField
+    scouter_name = forms.CharField(max_length=32, help_text="Please enter your name")
+    match_number = forms.IntegerField(help_text="enter Match Number",required=True)
+    team_number = forms.IntegerField(help_text="enter team number",required=True)
+    catches = forms.IntegerField(help_text="enter amount of catches",required=True)
+    truss = forms.IntegerField(help_text="enter number of truss shots", required = True) 
+    assists = forms.IntegerField(help_text="enter number of assists", required=True)
     
+    auto_points = forms.IntegerField(help_text="enter total autonomous points", required=True)
+    teleop_points = forms.IntegerField(help_text="enter total tele-op points", required=True)
+    
+    total_points = forms.IntegerField(help_text = "enter total points scored by robot, auto points plus tele-op points", required=True)
+
+    comments = forms.CharField(widget=forms.Textarea,help_text="Enter comments or stategy notes below:", required=False)
+
 
     class Meta:
         model = TeamData
